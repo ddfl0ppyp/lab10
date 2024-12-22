dc0cca8b1d2d5d96cf1491a07b54fdf90417d381
ddfl0ppyp <dontletthenightmaresbite@gmail.com>
2024-12-22 15:50:25 +0300

    init

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.java.lab10","l":"RaceManager.NoNameException"},{"p":"edu.java.lab10","l":"RaceManager"}];updateSearchResults();
